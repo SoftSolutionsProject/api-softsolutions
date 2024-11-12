57ff563a3369dd9fc1731730d7aaf277518c5c1f
LucasNotaro <lucas.sn@live.com>
2024-11-12 18:34:52 -0300

    remoção de arquivos nao mais usados

--- utils/validations.ts
+++
@@ -1,6 +0,0 @@
-export const validateInscricaoData = ({ _idModulo, _idUser }: { _idModulo: number; _idUser: number }): void => {
-    if (!_idModulo || !_idUser) {
-      throw new Error('Dados de inscrição incompletos');
-    }
-  };
-  
