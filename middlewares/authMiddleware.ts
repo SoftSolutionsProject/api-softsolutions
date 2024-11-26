9f0844f5dbe4a7610d2fbdc50a163c726e009921
LucasNotaro <lucas.sn@live.com>
2024-11-26 17:53:49 -0300

    deploy

--- middlewares/authMiddleware.ts
+++ middlewares/authMiddleware.ts
@@ -1,16 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
-import * as jwt from 'jsonwebtoken';
+import * as jwt from 'jsonwebtoken'; // Atualização aqui
 import { AppError } from '../utils/AppError';
 
+// Declaração de tipos no objeto `req` do Express
 declare module 'express-serve-static-core' {
   interface Request {
     user?: {
       _idUser: number;
       tipo: string;
-    }
+    };
   }
 }
 
+// Certifique-se de que a variável de ambiente `JWT_SECRET` está configurada
 const JWT_SECRET = process.env.JWT_SECRET || 'sua_chave_secreta';
 
 export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
@@ -23,7 +25,7 @@ export const authMiddleware = async (req: Request, res: Response, next: NextFunc
 
     // Verificação do token com tratamento de erro
     try {
-      const decoded = jwt.verify(token, JWT_SECRET) as { _idUser: number; tipo: string };
+      const decoded = jwt.verify(token, JWT_SECRET) as jwt.JwtPayload & { _idUser: number; tipo: string };
       req.user = decoded;
       next();
     } catch (error) {
