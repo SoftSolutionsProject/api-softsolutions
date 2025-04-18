a59f2cce8621ac3ce9458e685ba23bd2ba4d9051
LucasNotaro <lucas.sn@live.com>
2025-04-17 22:44:58 -0300

    feat(usuario): adiciona controller com rotas de criação e listagem

---
+++ src/usuario/dto/create-usuario.dto.ts
@@ -0,0 +1,21 @@
+export class CreateUsuarioDto {
+    nomeUsuario: string;
+    cpfUsuario: string;
+    email: string;
+    senha: string;
+    telefone?: string;
+    endereco?: {
+      rua?: string;
+      numero?: string;
+      bairro?: string;
+      cidade?: string;
+      estado?: string;
+      pais?: string;
+    };
+    localizacao?: {
+      type: 'Point';
+      coordinates: [number, number];
+    };
+    tipo?: 'aluno' | 'administrador';
+  }
+  
