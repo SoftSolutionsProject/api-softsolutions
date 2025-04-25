ab5cf85cbdc39593894062a48fc6e93e4605912b
LucasNotaro <lucas.sn@live.com>
2025-04-25 13:44:16 -0300

    refactor: padroniza uso do decorator @User e remove migration de inscrição não utilizada

--- src/interfaces/http/inscricao/inscricao.controller.ts
+++ src/interfaces/http/inscricao/inscricao.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Delete, Param, Body, Query, UseGuards, ForbiddenException } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Body, UseGuards, ForbiddenException } from '@nestjs/common';
 import { AuthGuard } from '../guards/auth.guard';
 import { InscricaoService } from '../../../application/inscricao/use-cases/inscricao.service';
 import { CreateInscricaoDto } from '../../../application/inscricao/dtos/create-inscricao.dto';
@@ -10,20 +10,29 @@ export class InscricaoController {
   constructor(private readonly service: InscricaoService) {}
 
   @Post()
-  async inscrever(@Body() dto: CreateInscricaoDto, @User() user: any) {
-    if (dto.idUser !== user.sub) throw new ForbiddenException('Acesso negado');
+  async inscrever(@Body() dto: CreateInscricaoDto, @User('sub') userId: number) {
+    if (dto.idUser !== userId) throw new ForbiddenException('Acesso negado');
     return this.service.inscrever(dto);
   }
 
   @Get(':idUser')
-  async listar(@Param('idUser') id: number, @User() user: any) {
-    if (id !== user.sub) throw new ForbiddenException('Acesso negado');
-    return this.service.listarPorUsuario(id);
-  }
+async listar(@Param('idUser') id: string, @User('sub') userId: number) {
+  const idNumber = Number(id);
+  if (idNumber !== userId) throw new ForbiddenException('Acesso negado');
+  return this.service.listarPorUsuario(idNumber);
+}
 
-  @Delete(':idUser/cursos/:idModulo')
-  async cancelar(@Param('idUser') id: number, @Param('idModulo') idModulo: number, @User() user: any) {
-    if (id !== user.sub) throw new ForbiddenException('Acesso negado');
-    return this.service.cancelar(id, idModulo);
-  }
+@Delete(':idUser/cursos/:idModulo')
+async cancelar(
+  @Param('idUser') id: string,
+  @Param('idModulo') idModulo: string,
+  @User('sub') userId: number,
+) {
+  const idNum = Number(id);
+  const idModuloNum = Number(idModulo);
+
+  if (idNum !== userId) throw new ForbiddenException('Acesso negado');
+  return this.service.cancelar(idNum, idModuloNum);
+}
+
 }
