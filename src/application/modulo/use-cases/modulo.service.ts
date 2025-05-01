b816b944018525b82a2cd33bdee21373f96e44bd
Evellin Lima <evellinlima01@gmail.com>
2025-04-30 22:26:07 -0300

    feat: implementar criação de módulo com validação do curso

--- src/application/modulo/use-cases/modulo.service.ts
+++ src/application/modulo/use-cases/modulo.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Modulo } from '../../../domain/modulo/modulo.entity';
 import { Curso } from '../../../domain/curso/curso.entity';
+import { CreateModuloDto } from '../dtos/create-modulo.dto';
 
 @Injectable()
 export class ModuloService {
@@ -12,4 +13,16 @@ export class ModuloService {
     @InjectRepository(Curso)
     private readonly cursoRepo: Repository<Curso>,
   ) {}
+
+  async create(dto: CreateModuloDto): Promise<Modulo> {
+    const curso = await this.cursoRepo.findOne({ where: { id: dto.idCurso } });
+    if (!curso) throw new NotFoundException('Curso não encontrado');
+
+    const modulo = this.moduloRepo.create({
+      nomeModulo: dto.nomeModulo,
+      tempoModulo: dto.tempoModulo,
+      curso,
+    });
+    return this.moduloRepo.save(modulo);
+  }
 }
