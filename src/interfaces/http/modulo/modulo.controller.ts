6968691a4623b33fb904f9a54ed802db0b3fa72a
Evellin Lima <evellinlima01@gmail.com>
2025-04-30 22:39:17 -0300

    feat: adicionar rotas GET para buscar todos os módulos e módulo por ID

--- src/interfaces/http/modulo/modulo.controller.ts
+++ src/interfaces/http/modulo/modulo.controller.ts
@@ -4,6 +4,8 @@ import { AuthGuard } from '../guards/auth.guard';
 import { Post, Body } from '@nestjs/common';
 import { CreateModuloDto } from '../../../application/modulo/dtos/create-modulo.dto';
 import { User } from '../usuario/decorators/user.decorator';
+import { Get, Param } from '@nestjs/common';
+
 
 @Controller('modulos')
 @UseGuards(AuthGuard)
@@ -15,4 +17,15 @@ async create(@Body() dto: CreateModuloDto, @User('tipo') tipo: string) {
     if (tipo !== 'administrador') throw new Error('Apenas administradores podem criar módulos');
     return this.moduloService.create(dto);
 }
+
+@Get()
+  async findAll() {
+    return this.moduloService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    return this.moduloService.findOne(id);
+  }
+
 }
