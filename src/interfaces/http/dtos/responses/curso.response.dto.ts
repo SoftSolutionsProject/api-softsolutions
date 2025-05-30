48447d3b34dd5c93f5e65013d32b0282ae22e16a
LucasNotaro <lucas.sn@live.com>
2025-05-30 17:02:03 -0300

    refactor(dto): renomeia campos no CursoResponseDto para manter consistência

--- src/interfaces/http/dtos/responses/curso.response.dto.ts
+++ src/interfaces/http/dtos/responses/curso.response.dto.ts
@@ -2,24 +2,24 @@ import { CursoModel } from 'src/domain/models/curso.model';
 
 export class CursoResponseDto {
   id: number;
-  nome: string;
-  tempo: number;
+  nomeCurso: string;
+  tempoCurso: number;
   descricaoCurta: string;
   professor: string;
   categoria: string;
   status: string;
   avaliacao: number;
-  imagem: string;
+  imagemCurso: string;
 
   constructor(curso: CursoModel) {
     this.id = curso.id!;
-    this.nome = curso.nomeCurso;
-    this.tempo = curso.tempoCurso;
+    this.nomeCurso = curso.nomeCurso;
+    this.tempoCurso = curso.tempoCurso;
     this.descricaoCurta = curso.descricaoCurta;
     this.professor = curso.professor;
     this.categoria = curso.categoria;
     this.status = curso.status;
     this.avaliacao = curso.avaliacao;
-    this.imagem = curso.imagemCurso;
+    this.imagemCurso = curso.imagemCurso;
   }
 }
